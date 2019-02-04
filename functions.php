2a78cdc31ed3e12adfc0c1e678598b8558b46d40
okahijiki <fu.tsurumaki@gmail.com>
2019-02-04 09:18:26 +0900

    コメントフォームを大幅に変更した｡
    １）メールとURLの記述欄を削除した｡
    ２）但し書き「メールアドレスが公開されることはありません」の文言を削除した｡
    ３）「コメントを残す」の文言を「公開まで､少し時間がかかりまーす!」に変更した｡
    ４）「名前」を「匿名でもOK」に変更した｡
    ５）「コメントを送信する」を「送信」に変更した｡

--- functions.php
+++ functions.php
@@ -7,3 +7,49 @@ function theme_enqueue_styles() {
         array('parent-style')
     );
 }
+
+/*既存の「メール」「URL」の記入欄を表示させない*/
+ function my_comment_form_fields( $fields){
+    unset( $fields['email']);   
+    unset( $fields['url']);     
+    return $fields;
+}
+add_filter( 'comment_form_default_fields', 'my_comment_form_fields');
+
+
+/*既存の但し書き「メールアドレスが公開されることはありません」を表示させない｡*/
+add_filter( "comment_form_defaults", "my_comment_notes_before");
+function my_comment_notes_before( $defaults){
+    $defaults['comment_notes_before'] = '';
+    return $defaults;
+}
+add_filter( "comment_form_defaults", "my_comment_notes_after");
+function my_comment_notes_after( $defaults){
+    $defaults['comment_notes_after'] = '';
+    return $defaults;
+}
+
+
+/*「コメントを残す」の文言を「公開まで､少し時間がかかりまーす｡」に変更｡*/
+add_filter( 'comment_form_defaults', 'my_title_reply');
+function my_title_reply( $defaults){
+    $defaults['title_reply'] = '公開に少し時間がかかります';
+    return $defaults;
+}
+
+
+/*名前を「とく名でもOK!」､「コメントを送信する」を「送信」に変えた*/
+function my_comment_form_defaults( $defaults ) {
+	$commenter = wp_get_current_commenter();
+	$req = get_option( 'require_name_email' );
+	$html_req = ( $req ? " required='required'" : '' );
+	$defaults['fields']['author'] = 
+		'<p class="comment-form-author">' . '<label for="author">匿名でOK !' . ( $req ? ' <span class="required">*</span>' : '' ) . '</label> ' .
+		'<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30" maxlength="245"' . $html_req . ' /></p>';
+	$defaults['label_submit'] = '送る';
+	return $defaults;
+}
+add_filter( 'comment_form_defaults', 'my_comment_form_defaults' );
+
+
+
