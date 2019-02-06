99ee2a713d7044f94f9678c362a133ad51d9fea4
okahijiki <fu.tsurumaki@gmail.com>
2019-02-06 12:49:00 +0900

    「好きの働き｡」をカスタム編集できるようにした｡

--- functions.php
+++ functions.php
@@ -52,4 +52,20 @@ function my_comment_form_defaults( $defaults ) {
 add_filter( 'comment_form_defaults', 'my_comment_form_defaults' );
 
 
+/*「好きの働き｡」をカスタム投稿にするコード*/
+function create_post_type() {
+  register_post_type( 'acme_product',
+    array(
+      'labels' => array(
+        'name' => __( '好きの働き｡' ),
+        'singular_name' => __( 'Product' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+    )
+  );
+}
+add_action( 'init', 'create_post_type' );
+
+
 
